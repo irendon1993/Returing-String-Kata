8f9eb99f14fc450e979350591ec704d28a708452
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2020-08-26 09:57:52 -0400

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class Kata
+{
+  public static string Greet(string name)
+  {
+    var message = $"Hello, {name} how are you doing today?" ; 
+    return message;
+  }
+}
